262c024049177d407ed6ba25b164356fdc2e9524
Nathan Rydel <nathanrydel@gmail.com>
2024-01-16 19:16:18 -0700

    autoformatter

--- coin-flip/src/coins.js
+++ coin-flip/src/coins.js
@@ -1,7 +1,10 @@
-import heads from "./heads.png"
-import tails from "./tails.png"
+import heads from "./heads.png";
+import tails from "./tails.png";
 
-const coins = [{imgSrc: heads, side: "heads"}, {imgSrc: tails, side: "tails"}];
+const coins = [
+  { imgSrc: heads, side: "heads" },
+  { imgSrc: tails, side: "tails" }
+];
 
-export { coins };
+export default coins;
 
